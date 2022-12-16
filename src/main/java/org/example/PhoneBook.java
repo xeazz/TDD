01426584bef3b91a82300da46fbdd851f31aadb9
Dmitri Vorobev <vorobiev1401@mail.ru>
2022-12-16 13:04:05 +0300

    Stub and tests for add

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,4 +1,8 @@
 package org.example;
 
 public class PhoneBook {
+
+    public int add (String name, String number) {
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,57 @@
+import org.example.PhoneBook;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PhoneBookTest {
+    PhoneBook phoneBook;
+
+    @BeforeAll
+    public static void iniSut() {
+        System.out.println("Running test");
+    }
+
+    @AfterAll
+    public static void completeSut() {
+        System.out.println("Test completed");
+    }
+
+    @BeforeEach
+    public void initEachTestFourth() {
+
+        phoneBook = new PhoneBook();
+        System.out.println(PhoneBook.class + " создан");
+    }
+
+    @AfterEach
+    public void afterEachTestFourth() {
+        phoneBook = null;
+        System.out.println(PhoneBook.class + " обнулен");
+    }
+
+    @Test
+    public void addContactFirstTest() {
+        String name = "Иван";
+        String phoneNumber = "88889992222";
+
+        int expected = 1;
+        int result = phoneBook.add(name, phoneNumber);
+
+        assertEquals(expected, result);
+    }
+    @Test
+    public void addContactSecondTest() {
+        phoneBook.add("Иван", "88889992222");
+        phoneBook.add("Павел", "88889992233");
+        phoneBook.add("Сергей", "88889992244");
+        phoneBook.add("Оля", "88889992255");
+
+        String name = "Таня";
+        String phoneNumber = "88889992266";
+
+        int expected = 5;
+        int result = phoneBook.add(name, phoneNumber);
+
+        assertEquals(expected, result);
+    }
 }
