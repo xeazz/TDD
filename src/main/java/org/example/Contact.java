9976c28c7b2e85867e291bf5df867e1bab87f7bc
Dmitri Vorobev <vorobiev1401@mail.ru>
2022-12-17 11:14:51 +0300

    Initial begin

---
+++ src/main/java/org/example/Contact.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Contact {
+
+    private final String name;
+    private final String phoneNumber;
+
+    public Contact(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+}
--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -4,24 +4,31 @@ import java.util.*;
 
 public class PhoneBook {
     private final TreeMap<String, String> book = new TreeMap<>();
+    private final List<Contact> listContact = new ArrayList<>();
+    private final Map<String, Integer> listName = new HashMap<>();
+    private final Map<String, Integer> listPhoneNumber = new HashMap<>();
+    private static int COUNT_LIST = 0;
+
 
     public int add(String name, String number) {
         book.put(name, number);
+        listContact.add(new Contact(name, number));
+        listName.put(name, COUNT_LIST);
+        listPhoneNumber.put(number, COUNT_LIST);
+        COUNT_LIST++;
         return book.size();
     }
 
-    public Object findByNumber(String number) {
-        Set<String> nameContact = book.keySet();
-        Object[] a = nameContact.toArray();
-        ArrayList<String> phoneNumber = new ArrayList<>(book.values());
-        if (phoneNumber.contains(number)) {
-            return a[phoneNumber.indexOf(number)];
-        }
-        return null;
+    public String findByNumber(String number) {
+        int numberPage = listPhoneNumber.get(number);
+        Contact list = listContact.get(numberPage);
+        return list.getName();
     }
 
     public String findByName(String name) {
-        return book.get(name);
+        int numberPage = listName.get(name);
+        Contact list = listContact.get(numberPage);
+        return list.getPhoneNumber();
     }
 
     public String printAllNames() {
